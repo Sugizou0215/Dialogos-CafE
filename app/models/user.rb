80a11478fd80ded85d425173c9e3f8946f45c20a
Sugizou0215 <sugizou0215@gmail.com>
2021-10-04 02:29:46 +0000

    ユーザー編集機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # バリデーション
+  validates :name, length: { minimum: 1 }
+
+  #ユーザー画像用（refile）
+  attachment :user_image
 end
