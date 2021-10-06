69423c994d2dd518fb61538ef4b01935bdc1ab75
Sugizou0215 <sugizou0215@gmail.com>
2021-10-06 11:02:56 +0000

    ブックマーク機能追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,13 +3,19 @@ class Event < ApplicationRecord
   has_many :users, through: :event_users ,dependent: :destroy
   belongs_to :genre
   has_many :event_comments, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
 
   #イベント画像用（refile）
   attachment :event_image
 
-  #simple_calender用のメソッド。start_timeが呼び出されると、start_atで設定して
+  #simple_calender用のメソッド。start_timeが呼び出されると、start_atで設定した時刻が返る
   def start_time
     self.start_at
+  end
+
+  #ブックマーク確認用
+  def bookmark_by?(user)
+    bookmarks.where(user_id: user.id).exists?
   end
 end
 
