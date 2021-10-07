ee4ed87c0a87d134b54de788d55ecb298ed194d6
Sugizou0215 <sugizou0215@gmail.com>
2021-10-07 02:16:13 +0000

    グループ新規作成機能追加

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,42 @@
+class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.admin_user_id = current_user.id
+    @group.users << current_user
+    if @group.save
+      redirect_to groups_path, notice: 'グループ作成に成功しました'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def index
+    @groups = Group.all
+  end
+
+  def edit
+    @group = Group.find(params:[id])
+  end
+
+  def update
+    if @group = Group.update(event_params)
+      redirect_to group_path(@group), notice: "正常にイベント情報が変更されました。"
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+    def group_params
+      params.require(:group).permit(:name, :introduction, :group_image)
+    end
+end
