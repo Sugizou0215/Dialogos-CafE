4b9eefdf46d77376ec97744324425ea7c34ac211
Sugizou0215 <sugizou0215@gmail.com>
2021-10-08 12:29:51 +0000

    マイページ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -59,5 +60,12 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :introduction, :user_image)
+    end
+
+    def ensure_correct_user
+      @user = User.find(params[:id])
+      unless @user == current_user
+        redirect_to user_path(current_user)
+      end
     end
 end
