e96576ed718c22b05f1c82eceb43cd1ee7d21557
Sugizou0215 <sugizou0215@gmail.com>
2021-10-12 02:36:40 +0000

    非同期通信機能追加

--- app/controllers/group_comments_controller.rb
+++ app/controllers/group_comments_controller.rb
@@ -3,22 +3,22 @@ class GroupCommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    group = Group.find(params[:group_id])
-    @comment = current_user.group_comments.new(group_comment_params)
-    @comment.group_id = group.id
-    @comment_group = @comment.group
-    if @comment.save
+    @group = Group.find(params[:group_id])
+    @group_comment = current_user.group_comments.new(group_comment_params)
+    @group_comment.group_id = @group.id
+    @comment_group = @group_comment.group
+    if @group_comment.save
       #通知の作成
-      @comment_group.create_notification_comment!(current_user, @comment)
-      redirect_to group_path(group)
+      @comment_group.create_notification_comment!(current_user, @group_comment)
+      # redirect_to group_path(group)
     end
   end
 
   def destroy
     @group = Group.find(params[:group_id])
-    group_comment = @group.group_comments.find(params[:id])
-    group_comment.destroy
-    redirect_to group_path(params[:group_id])
+    @group_comment = @group.group_comments.find(params[:id])
+    @group_comment.destroy
+    # redirect_to group_path(params[:group_id])
   end
 
   private
