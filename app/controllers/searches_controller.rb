7ba8fbc2bf2a97a6c4b949a6fffd0703d513f060
Sugizou0215 <sugizou0215@gmail.com>
2021-10-13 07:42:24 +0000

    ジャンル検索追加

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,8 @@ class SearchesController < ApplicationController
   def event
     @value = params['search']['value']
     if @value != ""
-      @events = Event.search_for(@value) #イベントモデル内のメソッド呼び出し
+      @events = Event.search_for(@value) #models/event.rb参照
+      @events = Kaminari.paginate_array(@events).page(params[:page]).per(10)
       @user = current_user #ユーザー情報表示用（サイドバー）
     else
       @events = Event.all
@@ -14,7 +15,15 @@ class SearchesController < ApplicationController
 
   def group
     @value = params['search']['value']
-    @groups = Group.search_for(@value) #イベントモデル内のメソッド呼び出し
+    @groups = Group.search_for(@value) #models/group.rb参照
+    @groups = Kaminari.paginate_array(@groups).page(params[:page]).per(10)
+    @user = current_user #ユーザー情報表示用（サイドバー）
+  end
+
+  def user
+    @value = params['search']['value']
+    @users = User.search_for(@value) #models/user.rb参照
+    @users = Kaminari.paginate_array(@users).page(params[:page]).per(10)
     @user = current_user #ユーザー情報表示用（サイドバー）
   end
 end
