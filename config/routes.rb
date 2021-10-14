b5890235bedcc3a27688e856ad0ba92b4e3c7fd7
Sugizou0215 <sugizou0215@gmail.com>
2021-10-14 05:33:56 +0000

    エラーメッセージ修正

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   }
   get "/confirm" => "users#confirm", as: 'users_confirm'
   put "/users/:id/leave" => "users#leave", as: 'users_leave'
-  get "/users" => "users#error"
+  get "/users" => "users#error" #hidden_field使用に伴うルーティングエラー用(新規登録)
   resources :users, only: [:show, :edit, :update] do
     resources :relationships, only: [:create, :destroy]
     get "followings" => 'relationships#followings', as: 'followings'
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
     delete "/notices/destroy_all" => 'notices#destroy_all'
   end
   resources :chats, only: [:show, :create]
-  get '/chats' => 'chats#error' #hidden_field使用に伴うルーティングエラー用
+  get '/chats' => 'chats#error' #hidden_field使用に伴うルーティングエラー用（チャット）
   patch "events/:id/cancel" => "events#cancel", as: 'cancel_event'
   resources :events, except: [:destroy] do
     get "join" => "events#join", as: 'join'
@@ -43,4 +43,5 @@ Rails.application.routes.draw do
   post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
   post 'contacts/back', to: 'contacts#back', as: 'back'
   get 'done', to: 'contacts#done', as: 'done'
+  match '*unmatched_route', :to => 'application#raise_not_found!', :via => :all #ルーティングエラー用
 end
