2229bb31337bd2c31be79d4bdffa12eeff23bcbe
Sugizou0215 <sugizou0215@gmail.com>
2021-10-20 12:18:42 +0000

    tagmapのevent_idをbigintに変更

--- db/migrate/20211009080610_create_tagmaps.rb
+++ db/migrate/20211009080610_create_tagmaps.rb
@@ -1,7 +1,7 @@
 class CreateTagmaps < ActiveRecord::Migration[5.2]
   def change
     create_table :tagmaps do |t|
-      t.references  :event,  index: true, foreign_key: true
+      t.bigint :event_id, index: true, foreign_key: true
       t.references  :tag, index: true, foreign_key: true
 
       t.timestamps
