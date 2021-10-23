e3b0cbd41db39801eb115bc25d812cf778c65514
Sugizou0215 <sugizou0215@gmail.com>
2021-10-23 05:30:17 +0000

    DB修正

--- db/migrate/20211009080630_create_tagmaps.rb
+++ db/migrate/20211009080630_create_tagmaps.rb
@@ -1,7 +1,8 @@
 class CreateTagmaps < ActiveRecord::Migration[5.2]
   def change
     create_table :tagmaps do |t|
-      t.bigint :event_id, index: true, foreign_key: true
+      t.references :event, index: true, foreign_key: true
+      #t.bigint :event_id, index: true, foreign_key: true
       t.references :tag, index: true, foreign_key: true
 
       t.timestamps
